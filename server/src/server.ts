1366fd1b518c64d8a7651d3f063046a82337f683
leandrosouzaa <leandror.dev@gmail.com>
2020-08-08 11:40:27 -0300

    feat: create node server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World2')
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+   return res.json({message: 'olá mundo'})
+})
+
+app.listen(3333, () => console.log('👷 Server Started in port 3333') )
