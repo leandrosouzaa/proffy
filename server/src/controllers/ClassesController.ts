ca03e24d9c995a378f2e061ac80bd1f687be462b
leandrosouzaa <leandror.dev@gmail.com>
2020-08-08 20:55:44 -0300

    fix: change 'to' to 'from' in classes query

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -33,11 +33,7 @@ export default class ClassesController {
                .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
                .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
                .whereRaw('`class_schedule`.`from` <=  ??', [timeInMinutes])
-<<<<<<< HEAD
-               .whereRaw('`class_schedule`.`from` > ??', [timeInMinutes])
-=======
-               .whereRaw('`class_schedule`.`from` < ??', [timeInMinutes])
->>>>>>> 4ba97dd0c76fde7cea55a744eecf7c49b2998642
+               .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
          })
          .where('classes.subject', '=', subject)
          .join('users', 'classes.user_id', '=', 'users.id')
