cadb64a4ec53f46fdb766aeab8496f8da00a7938
leandrosouzaa <leandror.dev@gmail.com>
2020-08-08 20:49:14 -0300

    feat: create connections list and create connection

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionsController()
 
 const routes = express.Router();
 
 routes.post('/classes', classesControllers.create);
 routes.get('/classes', classesControllers.index);
 
+routes.get('/connections',connectionsControllers.index);
+routes.post('/connections',connectionsControllers.create);
 
 export default routes;
