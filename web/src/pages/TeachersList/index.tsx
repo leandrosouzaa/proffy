2aae405575e57cdfd8927046cb249f742f74e315
leandrosouzaa <leandror.dev@gmail.com>
2020-08-07 23:06:27 -0300

    feat: finish TeachersList page (only design)

--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const TeachersList: React.FC = () => {
-  return <div />;
-}
+import { PageHeader, TeacherItem } from '../../components';
 
-export default TeachersList;
+import './styles.css';
+
+const TeachersList: React.FC = () => {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis :)">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" name="subject" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" name="week_day" id="week_day"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" name="time" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+
+      </main>
+    </div>
+    )
+  }
+  
+  export default TeachersList;
